acf12311dd268a4fcee9b39564466b34a1b8fe22
elhadjhamlaoui <elhadjhamlaoui@gmail.com>
2019-05-21 04:29:40 +0200

    fixed java.lang.OutOfMemoryError

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 if [ $# -eq 0 ]
   then
-    mvn exec:java -Dexec.mainClass="org.jitsi.sphinx4http.server.HttpServer"
+    mvn exec:java -Dexec.mainClass="org.jitsi.sphinx4http.server.HttpServer" -Xmx2048m
+
   else
-    mvn exec:java -Dexec.mainClass="org.jitsi.sphinx4http.server.HttpServer" -Dexec.args="$1"
+    mvn exec:java -Dexec.mainClass="org.jitsi.sphinx4http.server.HttpServer" -Dexec.args="$1" -Xmx2048m 
 fi
 
 
