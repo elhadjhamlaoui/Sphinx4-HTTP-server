c63e1f4e37132d11fc0ec6e1346eab582194332b
elhadjhamlaoui <elhadjhamlaoui@gmail.com>
2019-05-21 04:56:55 +0200

    fixing heap size problem

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 if [ $# -eq 0 ]
   then
-    mvn exec:java -Xmx2048m -Dexec.mainClass="org.jitsi.sphinx4http.server.HttpServer" 
+    mvn exec:java -Dexec.mainClass="org.jitsi.sphinx4http.server.HttpServer" 
 
   else
-    mvn exec:java -Xmx2048m -Dexec.mainClass="org.jitsi.sphinx4http.server.HttpServer" -Dexec.args="$1" 
+    mvn exec:java -Dexec.mainClass="org.jitsi.sphinx4http.server.HttpServer" -Dexec.args="$1" 
 fi
 
 
